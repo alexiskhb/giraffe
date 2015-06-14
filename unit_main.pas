04c87a520be13c16ae94df42e93e23122e62ef77
Shchurov Alexey <alexiskhb@gmail.com>
2015-06-14 21:44:51 +1000

    start

---
+++ unit_main.pas
@@ -0,0 +1,23 @@
+unit unit_main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm1 = class(TForm)
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
