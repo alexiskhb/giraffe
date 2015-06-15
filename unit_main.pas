93c1eb5354ae19999accc0300b4c774a6d97ec98
Shchurov Alexey <alexiskhb@gmail.com>
2015-06-15 10:22:50 +1000

    + not working left-num-analyze

--- unit_main.pas
+++ unit_main.pas
@@ -39,6 +39,8 @@ type
   public
     function Analyze(ANewChar: char; var ACurLength: integer;
       var ADirection: TDirection): boolean;
+    function AnalyzeNumFirst(ANewChar: char; var ACurLength: integer;
+      var ADirection: TDirection): boolean;
     constructor Create;
   end;
 
@@ -97,6 +99,92 @@ implementation
 { TMainForm }
 
 function TDetermFigureFSM.Analyze(ANewChar: char; var ACurLength: integer;
+  var ADirection: TDirection): boolean;
+begin
+  Result := false;
+  {CurState = reading num}
+  if FCurState = msNum then
+    if ANewChar in ValidIntegers then begin
+      FCurNum := FCurNum*10 + StrToInt(ANewChar);
+      exit(false);
+    end
+    else begin
+      FCurState := msStart;
+      ACurLength := FCurNum;
+      FCurNum := 0;
+      exit(true);
+    end;
+
+  {CurState = reading first char}
+  if FCurState = msFirstChar then begin
+    case FCurChars of
+        'r', 'R': ADirection := dRight;
+        'd', 'D': ADirection := dDown;
+        'l', 'L': ADirection := dLeft;
+        'u', 'U': ADirection := dUp;
+    end;
+    if ANewChar in ValidDirectionChars then begin
+      FCurChars := FCurChars + ANewChar;
+      FCurState := msSecondChar;
+      exit(false);
+    end
+    else
+    if ANewChar in ValidIntegers then begin
+      FCurNum := StrToInt(ANewChar);
+      FCurState := msNum;
+      FCurChars := '';
+      exit(false);
+    end
+    else begin
+      FCurState := msStart;
+      FCurChars := '';
+      exit(false);
+    end;
+  end;
+
+  {CurState = reading second char}
+  if FCurState = msSecondChar then begin
+    case FCurChars of
+      'rr', 'RR': ADirection := dRight;
+      'dd', 'DD': ADirection := dDown;
+      'll', 'LL': ADirection := dLeft;
+      'uu', 'UU': ADirection := dUp;
+      'ru', 'rU', 'Ru', 'RU', 'ur', 'uR', 'Ur', 'UR': ADirection := dRightUp;
+      'rd', 'rD', 'Rd', 'RD', 'dr', 'dR', 'Dr', 'DR': ADirection := dRightDown;
+      'lu', 'lU', 'Lu', 'LU', 'ul', 'uL', 'Ul', 'UL': ADirection := dLeftUp;
+      'ld', 'lD', 'Ld', 'LD', 'dl', 'dL', 'Dl', 'DL': ADirection := dLeftDown;
+      else ADirection := dNone;
+    end;
+    if ANewChar in ValidIntegers then begin
+      FCurState := msNum;
+      FCurNum := StrToInt(ANewChar);
+      FCurChars := '';
+      exit(false);
+    end
+    else begin
+      FCurState := msStart;
+      FCurChars := '';
+      exit(false);
+    end;
+  end;
+
+  {CurState = reading start}
+  if FCurState = msStart then begin
+    if ANewChar in ValidDirectionChars then begin
+      FCurChars := ANewChar;
+      FCurState := msFirstChar;
+      exit(false);
+    end
+    else
+    if ANewChar in ValidIntegers then begin
+      FCurNum := StrToInt(ANewChar);
+      FCurState := msNum;
+      exit(false);
+    end;
+  end;
+end;
+
+function TDetermFigureFSM.AnalyzeNumFirst(ANewChar: char; var ACurLength: integer;
   var ADirection: TDirection): boolean;
 begin
   Result := false;
